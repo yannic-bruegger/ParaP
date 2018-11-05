44a673cad226e1f0dcadf4db69608e448bdbb61e
levy <yannic.bruegger@gmail.com>
2018-11-05 23:31:37 +0100

    Test Commit

--- src/application/TestApplication.java
+++ src/application/TestApplication.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 
 public class TestApplication
 {
-    private static final int CRITERIAOPTION = 8;
+    private static final int CRITERIAOPTION = 5;
 
     public static void main( String[] args )
     {
